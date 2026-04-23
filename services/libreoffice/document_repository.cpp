ec603d382c27f8eb6eeaa8f15f4c2cf5ea2bcca3
treedev <edw.pener@gmail.com>
2026-04-24 00:40:31 +0300

    development

--- services/libreoffice/document_repository.cpp
+++ services/libreoffice/document_repository.cpp
@@ -10,7 +10,14 @@ LibreOfficeDocumentRepository &LibreOfficeDocumentRepository::getInstance() {
 }
 
 std::string LibreOfficeDocumentRepository::registerDocument(const std::string &name) {
-    uuids::uuid_system_generator gen;
+    std::random_device rd;
+    auto seed_data = std::array<int, std::mt19937::state_size> {};
+
+    std::ranges::generate(seed_data, std::ref(rd));
+    std::seed_seq seq(std::begin(seed_data), std::end(seed_data));
+    std::mt19937 generator(seq);
+    uuids::uuid_random_generator gen{generator};
+
     const auto uuid_ = uuids::to_string(gen());
     std::lock_guard lock_guard(mutex_);
     documents[uuid_] = std::make_unique<LibreOfficeDocument>(name);
